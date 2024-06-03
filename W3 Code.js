a09913084c3bffd3323578102c3e556f844b079c
Mags <144168396+Magsaroni@users.noreply.github.com>
2024-06-03 02:59:45 -0700

    Add files via upload

---
+++ W3 Code.js
@@ -0,0 +1,9 @@
+function setPageInfo(){
+    document.getElementById("h1PageTitle").textContent = "Magaly's Bio Info";
+    document.getElementById("divMajor").textContent = "Major: Advertising Arts (AA)";
+    document.getElementById("divEmail").textContent = "Email: Mvaldez85055@uat.edu";
+    document.getElementById("divGraduation").textContent = "Graduation Date: Early 2026(ish)";
+    document.getElementById("divAge").textContent = "Age: 18";
+
+    console.log("sucess!");
+}
